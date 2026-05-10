5549c5ac91b13d13561730dc0c9762801c6baca3
liansongye <liansongye@bytedance.com>
2026-05-10 17:00:19 +0800

    fix: add homepage avatar asset

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
@@ -8,23 +9,23 @@ hugo --cleanDestinationDir
 # Sync the generated site and delete files that no longer exist locally.
 rsync -av --delete --exclude='.git/' public/ ../../hugo_public/yeliansong.github.io/
 
-# Go To Public folder
+# Go to the GitHub Pages repository.
 cd ../../hugo_public/yeliansong.github.io/
-# Add changes to git.
+
+# Add generated changes to git.
 git add .
 
-# Commit changes.
-msg="rebuilding site `date` "
+msg="rebuilding site $(date)"
+
+if [ $# -eq 1 ]; then
+  msg="$1"
+fi
 
 echo -e "\033[0;32m$msg\033[0m"
 
-if [ $# -eq 1 ]
-  then msg="$1"
+if git diff --cached --quiet; then
+  echo -e "\033[0;33mNo generated changes to deploy.\033[0m"
+else
+  git commit -m "$msg"
+  git push origin master
 fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
-# Come Back up to the Project Root
-cd ..
